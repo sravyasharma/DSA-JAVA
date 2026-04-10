fac1e852e2865b350b210f32bfdce3afcf0aa74d
Devalla Lakshmi Sai Sravya <sravyadevallalaxmi@gmail.com>
2026-04-10 23:41:24 +0530

    Time: 4 ms (99.76%), Space: 47.7 MB (56.75%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left=0,right=s.length()-1,count=0;
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right)) return isPal(s,left+1,right) || isPal(s,left,right-1);
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean isPal(String s,int left,int right){
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
