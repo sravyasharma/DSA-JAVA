fe27b7e9397faa8c0dd6f7abfade789793848ee7
Devalla Lakshmi Sai Sravya <sravyadevallalaxmi@gmail.com>
2026-05-11 01:07:45 +0530

    Time: 14 ms (80.24%), Space: 47.2 MB (14.97%) - LeetHub

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        for(int i=0;i<=nums.length-1;i++){
+            if(i>0 && dp[i]<=0) continue;
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(Math.abs(nums[j]-nums[i])<=Math.abs(target)) dp[j]=Math.max(dp[j],dp[i]+1);
+            }
+        }
+        return dp[nums.length-1];
+    }
+}
