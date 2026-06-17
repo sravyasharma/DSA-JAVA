70df0ec43a0488ef3b650e2b715388792fe9eb0d
Devalla Lakshmi Sai Sravya <sravyadevallalaxmi@gmail.com>
2026-06-17 21:14:13 +0530

    Time: 31 ms (53.96%), Space: 48.5 MB (67.12%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : candyType) set.add(i);
+        return Math.min(set.size(), candyType.length/2);
+    }
+}
