671ac9db37b07a033f3dbfe0b702f66af4b56895
Devalla Lakshmi Sai Sravya <sravyadevallalaxmi@gmail.com>
2026-06-27 22:51:40 +0530

    Time: 6 ms (23.23%), Space: 46.2 MB (6.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,14 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        
+    public static boolean isPalindrome(int x) {
+            String s=String.valueOf(x);
+            int left=0;int right=s.length()-1;
+            while(left<right){
+                if(s.charAt(left)!=s.charAt(right)){
+                    return false;
+                }
+                left++;
+                right--;
+            }   
+       return true; 
     }
 }
